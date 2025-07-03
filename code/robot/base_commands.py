40918ac97881e531c89623d62306711bb276269c
avriella-j <ajimenez2@students.rj.nsw.edu.au>
2025-07-03 11:24:22 +1000

    Update base_commands.py

--- code/robot/base_commands.py
+++ code/robot/base_commands.py
@@ -1,4 +1,4 @@
-from ..config.settings_controls import RobotDogSettings
+from config.settings_controls import RobotDogSettings
 
 class BaseCommands:
     def __init__(self, settings: RobotDogSettings):
